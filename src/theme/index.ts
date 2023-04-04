ac38fa97732907eb169902c5480b26a0dc1d4f64
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-04-04 10:50:29 -0300

    chore: setup styled-components and import of assets

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,11 +1,11 @@
 export default {
   'red-dark': '#BF3B44',
-  'gray-1': '#1B1D1E',
-  'gray-2': '#333638',
-  'gray-3': '#5C6265',
-  'gray-4': '#B9BBBC',
-  'gray-5': '#DDDEDF',
-  'gray-6': '#DDDEDF',
-  'gray-7': '#FAFAFA',
+  'gray-100': '#1B1D1E',
+  'gray-200': '#333638',
+  'gray-300': '#5C6265',
+  'gray-400': '#B9BBBC',
+  'gray-500': '#DDDEDF',
+  'gray-600': '#DDDEDF',
+  'gray-700': '#FAFAFA',
   white: '#FFFFFF',
 }
