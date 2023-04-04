cae84b5b78d2bb48bda56a687045788804fd038a
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-04-03 21:59:53 -0300

    chore: initial setup

---
+++ src/theme/index.ts
@@ -0,0 +1,11 @@
+export default {
+  'red-dark': '#BF3B44',
+  'gray-1': '#1B1D1E',
+  'gray-2': '#333638',
+  'gray-3': '#5C6265',
+  'gray-4': '#B9BBBC',
+  'gray-5': '#DDDEDF',
+  'gray-6': '#DDDEDF',
+  'gray-7': '#FAFAFA',
+  white: '#FFFFFF',
+}
