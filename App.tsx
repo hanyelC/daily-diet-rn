2fdd348c9cf5f5bfb86bf80d55eed7dcf54e6a79
Hanyel Chamon <hanyel.chamon@gmail.com>
2023-04-05 15:38:05 -0300

    feat: load fonts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,17 @@
 import { Home } from '@screens'
 import theme from '@theme'
 
+import {
+  // eslint-disable-next-line camelcase
+  NunitoSans_400Regular,
+  // eslint-disable-next-line camelcase
+  NunitoSans_700Bold,
+  useFonts,
+} from '@expo-google-fonts/nunito-sans'
+
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { ThemeProvider } from 'styled-components/native'
+import { ActivityIndicator } from 'react-native'
 
 // eslint-disable-next-line no-undef
 if (__DEV__) {
@@ -10,6 +19,15 @@ if (__DEV__) {
 }
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    // eslint-disable-next-line camelcase
+    NunitoSans_400Regular,
+    // eslint-disable-next-line camelcase
+    NunitoSans_700Bold,
+  })
+
+  if (!fontsLoaded) return <ActivityIndicator />
+
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
